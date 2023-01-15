828d713dbdd501b3c88983bf025bcb4687eeff0a
Merdobeyims <122176267+Merdobeyims@users.noreply.github.com>
2023-01-15 21:17:12 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("session_name", "BACM01YnPd5ku2fcBtgmeiWWMhsoV4ocNPPDwh2qsTNLWp0roVRp8VNjA9F4zXJk3Qm5pUGuCjYnLrxTyRq92yz_yXn6pomxaV-5VIhOBF1U2gVBgv1ozlCDozEuqJnOuTteibKwg7v1Z5IgwWGVLRrH-HmYnNUPM9FzEof_autmHndfYS3NYzZ2_g92KAgyLh4_zqNDVtyu2oHcDHszYn-FmYDlo3G8VyPpWKzkmRSJsDp8LpODfclUqRixiSrhY9Kpir9g3YDk8Q0tjGJAT8gYJVmc-OFwjK_s2q_os23d05K_yPLa0y12p6h2sNJ2yb9ockqgEvcTEY5ASnQiPMg-AAAAAVHgN2AA")
+SESSION_NAME = getenv("session_name", "BACZ4OcJ1XnS0ujrKs2ek9fwDwSXwqp_gz5g2MGZWIFA7OhIf-2lL4gIpFNvLwsHtFkk8d9X_65zsHOH1b93fzwGs-lGhHcAsx3ccsvE1OmPJ3V_c9R2nZAjeb5xFiMCVF4MGn1taH2aZ_A-Tp2_Y-khz165hQxWYWwx_m2TXaIK94xvUeholqufLjtFCnl5X9qo210YijW8GrpJJXla7LJlsPQms7-gWPoWKgQWSPLnuMCw4HmgaDCu784gnPb5iVyAsNtAO90pl6HLeghq7TF_Em4yg52L_xBsM95WRRSQsW-TSZ5pBAtzbJLi2QvzOZdZYhtRIxB0STyuF-h8-uh0UhAOHgA")
 API_ID = int(getenv("api_id", "9316256"))
 API_HASH = getenv("api_hash", "5a8a277605c3038129c536a9e79cd761")
 CHANNEL_ID = getenv("channel_id", "-1001853845425")
