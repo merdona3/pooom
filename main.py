5240de33e6745e896e83018689992c628dfe2284
merdoobeyims <122889450+merdoobeyims@users.noreply.github.com>
2023-04-15 23:14:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ logging.basicConfig(
 
 LOGGER = logging.getLogger(__name__)
 
-app = Client("SongPlayRoBot", session_name=SESSİON_NAME, api_hash=API_HASH, api_id=API_ID) 
+app = Client("SongPlayRoBot", session_name=SESSION_NAME, api_hash=API_HASH, api_id=API_ID) 
 
 async def msg_info(msg):
     media_type = ""
